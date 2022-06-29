f71f8e7d10e14b2e75bb8b28a07d85abef3d60cd
willyc <willcowendrums@gmail.com>
2022-06-29 09:41:41 +0100

    towers update

---
+++ src/medium/towersOfHanoi/solution.js
@@ -0,0 +1,19 @@
+const towersOfHanoi = (discs) => {
+
+    let towerOne = []
+    let towerTwo = []
+    let towerThree = []
+
+    //initialise game with discs array. Biggest disc to smallest [3, 2, 1, 0].
+
+    for(let i = discs; i >= 1; i--) {
+    towerOne.push(i)
+    }
+    
+
+    towerOne.forEach(function(disc) {
+        
+    })
+}
+
+towersOfHanoi(3)
