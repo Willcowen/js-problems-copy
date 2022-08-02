5aa5b3d665e98460277aa0861f75536f55e3915b
willyc <willcowendrums@gmail.com>
2022-08-02 13:42:55 +0100

    added random solution

---
+++ src/easy/random/solution.js
@@ -0,0 +1,17 @@
+/* # Random Number Generator
+
+Create your own method for generating a random number between two other numbers.
+
+It is common to use _time_ as a means of generating a random number, but there are other ways, too - can you find one?
+*/
+
+function RandomNumberGenerator (a, b) {
+    console.log(a, b)
+    const min = Math.ceil(a);
+    const max = Math.floor(b);
+    const random =  Math.floor(Math.random() * (max - min) + min)
+    console.log(random)
+    return random; //The maximum is exclusive and the minimum is inclusive
+}
+
+RandomNumberGenerator(1, 100)
