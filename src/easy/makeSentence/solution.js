19d1ab2472a7bf2750ceb03f6b52574af401fabd
willyc <willcowendrums@gmail.com>
2022-08-26 13:15:23 +0100

    makeSentence solution added.

---
+++ src/easy/makeSentence/solution.js
@@ -0,0 +1,13 @@
+function makeSentence (string) {
+const punctuation = [',', '.', '!', ';']
+string = string.charAt(0).toUpperCase() + string.slice(1)
+if (punctuation.includes(string.charAt(string.length - 1))) {
+    return string
+}
+
+else {
+    return string + '.'}
+}
+
+makeSentence('hello there, my name is Will!')
+makeSentence('hello there, my name is Will')
