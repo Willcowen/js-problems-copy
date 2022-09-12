b1984d8372c73b5c1e85de6e3372231e94586b3c
Will <willcowendrums@gmail.com>
2022-09-12 15:14:48 +0100

    roman numerals solution added

--- src/medium/romanNumerals/solution.js
+++ src/medium/romanNumerals/solution.js
@@ -1,19 +1,28 @@
 const romanNumerals = (romanNum) => {
-const numerals = {
+  const numerals = {
     I: 1,
     X: 10,
     C: 100,
     M: 1000,
-}
-let total = 0
-console.log(romanNum)
-const arr = romanNum.split('')
-console.log('arr:', arr)
-arr.forEach((element, index) => {
-    console.log(index)
-    total += numerals[element]
-})
-
-console.log(total)
-}
-romanNumerals('XII')
+    V: 5,
+    L: 50,
+    D: 500,
+  };
+  let total = 0;
+  let values = [];
+  const arr = romanNum.split("");
+  arr.forEach((element) => {
+    values.push(numerals[element]);
+  });
+  for (let i = 0; i < values.length; i++) {
+    const currentValue = values[i];
+    const nextValue = values[i + 1];
+    if (currentValue < nextValue) {
+      total -= currentValue;
+    } else if (currentValue >= nextValue || nextValue === undefined) {
+      total += currentValue;
+    }
+  }
+  return total;
+};
+romanNumerals("MCMXCIV");
